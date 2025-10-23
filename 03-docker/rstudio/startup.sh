efe5d57ba849339d756fc89dd4055e36a91ed2d9
Mike Monaco <mamonaco1973@gmail.com>
2025-10-23 19:55:13 -0400

    fix logging

--- 03-docker/rstudio/startup.sh
+++ 03-docker/rstudio/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "=== Starting RStudio Server (forcing visible logs) ==="
+echo "NOTE: Starting RStudio Server (forcing visible logs)..."
 
-/usr/lib/rstudio-server/bin/rserver --server-daemonize=0 \
-  2>&1 | tee /var/log/rstudio/rstudio-server/rserver.log /proc/1/fd/1
+/usr/lib/rstudio-server/bin/rserver --server-daemonize=0 
