8f4f9054b3d9a2256fdcf3aa8cbc699da3562b2f
Mike Monaco <mamonaco1973@gmail.com>
2025-10-24 10:19:24 -0400

    capture logs properly

--- 03-docker/rstudio/startup.sh
+++ 03-docker/rstudio/startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -euo pipefail
+touch /var/log/rstudio/rstudio-server/rserver.log
 /usr/lib/rstudio-server/bin/rserver --server-daemonize=0 &
 tail -f /var/log/rstudio/rstudio-server/rserver.log 
+
+
