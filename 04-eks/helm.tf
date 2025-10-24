157cfd2722c4f806bca24a916a6e4f1b2ae65533
Mike Monaco <mamonaco1973@gmail.com>
2025-10-24 14:23:35 -0400

    provision eks cluster

--- 04-eks/helm.tf
+++ 04-eks/helm.tf
@@ -4,11 +4,7 @@
 ##############################################
 
 provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.rstudio_eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.rstudio_eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.rstudio_eks.token
-  }
+  kubernetes = kubernetes.eks
 }
 
 ##############################################################
